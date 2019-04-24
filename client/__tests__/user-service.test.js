7906b8e02e6a545496cde94d7b22628817d213c7
Anshu Dutta <anshudutta@Anshus-MacBook-Pro-2.local>
2019-04-24 13:09:51 +1000

    Fixed consumer url

--- client/__tests__/user-service.test.js
+++ client/__tests__/user-service.test.js
@@ -1,26 +1,26 @@
-const provider = require('../mock-server/provider');
-const { getAllUsers } = require('../services/user-service');
-const interactions = require('../mock-server/interactions');
+const provider = require("../mock-server/provider");
+const { getAllUsers } = require("../services/user-service");
+const interactions = require("../mock-server/interactions");
 
-describe('user-service', () => {
-	beforeAll(async () => {
-		process.env['SERVICE_URL'] = 'http://localhost:1234';
-		console.log('starting provider...');
-		await provider.setup();
-		console.log('started provider.');
-	});
+describe("user-service", () => {
+  beforeAll(async () => {
+    process.env["SERVICE_URL"] = "http://localhost:1234";
+    console.log("starting provider...");
+    await provider.setup();
+    console.log("started provider.");
+  });
 
-	afterAll(async () => {
-		console.log('shutting down provider...');
-		await provider.finalize();
-		console.log('provider shut down.');
-	});
+  afterAll(async () => {
+    console.log("shutting down provider...");
+    await provider.finalize();
+    console.log("provider shut down.");
+  });
 
-	describe('getAllUser', () => {
-		it('should get list from server', async () => {
-			await provider.addInteraction(interactions.getAllUsers);
-			const users = await getAllUsers();
-			await provider.verify();
-		});
-	});
+  describe("getAllUser", () => {
+    it("should get list from server", async () => {
+      await provider.addInteraction(interactions.getAllUsers);
+      const users = await getAllUsers("http://localhost:1234");
+      await provider.verify();
+    });
+  });
 });
